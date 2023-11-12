f2fe7b9d34d08f16f590c8420c6172412c568d30
nlunce <naterrl15@byui.edu>
2023-11-11 18:02:00 -0700

    pages

--- frontend/src/pages/EventDetail.js
+++ frontend/src/pages/EventDetail.js
@@ -1,4 +1,5 @@
-import { useParams, Link } from "react-router-dom";
+import { useParams, json } from "react-router-dom";
+import EventItem from "../components/EventItem";
 
 export default function EventDetailPage() {
   const params = useParams();
@@ -6,12 +7,20 @@ export default function EventDetailPage() {
   return (
     <>
       <h1>Event Details!</h1>
-      <p>{params.eventId}</p>
-      <p>
-        <Link to=".." relative="path">
-          Back
-        </Link>
-      </p>
+      <EventItem event={}/>
     </>
   );
+}
+
+export async function loader({request, params}){
+  const id = params.eventId
+  
+  const response = await fetch("http://localhost:8080/events/" + id);
+
+  if(!response.ok){
+    throw json({message: 'Could not fetch details for selected event.'},{status:500})
+  }else{
+    return response;
+  }
+
 }
--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,9 +1,13 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, json } from "react-router-dom";
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
   const data = useLoaderData();
   const events = data.events;
+
+  // if (data.isError) {
+  //   return <p>{data.message}</p>;
+  // }
   return (
     <>
       <EventsList events={events} />
@@ -17,6 +21,11 @@ export async function loader() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
+    // return { isError: true, message: "Could not fetch events." };
+    // throw new Response(
+    //   JSON.stringify({ message: "Could not fetch events.", status: 500 })
+    // );
+    return json({ message: "Could not fetch events" }, { status: 500 });
   } else {
     return response;
   }
