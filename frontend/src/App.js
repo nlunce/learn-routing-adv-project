99d6b76ee46676ad029ecf8d6bed4528b6fbfccb
nlunce <nluncef@gmail.com>
2023-11-11 20:41:15 -0700

    newsletter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,18 @@
-// Challenge / Exercise
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import HomePage from "./pages/Home";
-import EventsPage, { loader as eventsLoader } from "./pages/Events";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import EditEventPage from "./pages/EditEvent";
+import ErrorPage from "./pages/Error";
 import EventDetailPage, {
   loader as eventDetailLoader,
   action as deleteEventAction,
 } from "./pages/EventDetail";
-import NewEventPage, { action as newEventAction } from "./pages/NewEvent";
-import EditEventPage from "./pages/EditEvent";
-import RootLayout from "./pages/Root";
+import EventsPage, { loader as eventsLoader } from "./pages/Events";
 import EventsRootLayout from "./pages/EventsRoot";
-import ErrorPage from "./pages/Error";
+import HomePage from "./pages/Home";
+import NewEventPage from "./pages/NewEvent";
+import RootLayout from "./pages/Root";
+import { action as manipulateEventAction } from "./components/EventForm";
+import NewsletterPage, { action as newsletterAction } from "./pages/Newsletter";
 
 const router = createBrowserRouter([
   {
@@ -23,7 +25,11 @@ const router = createBrowserRouter([
         path: "events",
         element: <EventsRootLayout />,
         children: [
-          { index: true, element: <EventsPage />, loader: eventsLoader },
+          {
+            index: true,
+            element: <EventsPage />,
+            loader: eventsLoader,
+          },
           {
             path: ":eventId",
             id: "event-detail",
@@ -34,11 +40,24 @@ const router = createBrowserRouter([
                 element: <EventDetailPage />,
                 action: deleteEventAction,
               },
-              { path: "edit", element: <EditEventPage /> },
+              {
+                path: "edit",
+                element: <EditEventPage />,
+                action: manipulateEventAction,
+              },
             ],
           },
-          { path: "new", element: <NewEventPage />, action: newEventAction },
+          {
+            path: "new",
+            element: <NewEventPage />,
+            action: manipulateEventAction,
+          },
         ],
+      },
+      {
+        path: "newsletter",
+        element: <NewsletterPage />,
+        action: newsletterAction,
       },
     ],
   },
--- frontend/src/components/EventsNavigation.js
+++ frontend/src/components/EventsNavigation.js
@@ -1,36 +1,49 @@
-import classes from "./EventsNavigation.module.css";
 import { NavLink } from "react-router-dom";
 
-function EventsNavigation() {
+import classes from "./MainNavigation.module.css";
+import NewsletterSignup from "./NewsletterSignup";
+
+function MainNavigation() {
   return (
     <header className={classes.header}>
       <nav>
         <ul className={classes.list}>
           <li>
             <NavLink
-              to="/events"
+              to="/"
               className={({ isActive }) =>
                 isActive ? classes.active : undefined
               }
               end
             >
-              All Events
+              Home
             </NavLink>
           </li>
           <li>
             <NavLink
-              to="/events/new"
+              to="/events"
               className={({ isActive }) =>
                 isActive ? classes.active : undefined
               }
             >
-              New Event
+              Events
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/newsletter"
+              className={({ isActive }) =>
+                isActive ? classes.active : undefined
+              }
+            >
+              Newsletter
             </NavLink>
           </li>
         </ul>
       </nav>
+      <NewsletterSignup />
     </header>
   );
 }
 
-export default EventsNavigation;
+export default MainNavigation;
